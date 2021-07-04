356f7ec876a1fc7969c4cf8798c6cd08e87eb345
Louis Whisenant <louiswhisenant@gmail.com>
2021-07-03 23:12:13 -0500

    end section 3

---
+++ routes/api/auth.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// @route   GET api/auth
+// @desc    Test route
+// @access  Private
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route   POST api/auth
+// @desc    Login user
+// @access  Public
+router.post(
+	'/',
+	[
+		body('email', 'Valid email required').isEmail(),
+		body('password', 'Password is required').exists(),
+	],
+	async (req, res) => {
+		// set validationResult
+		const errors = validationResult(req);
+		// check for validation errors
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		// else
+		const { email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			if (!user) {
+				return res.status(400).json({
+					errors: [{ msg: 'Invalid credentials' }],
+				});
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) {
+				return res.status(400).json({
+					errors: [{ msg: 'Invalid credentials' }],
+				});
+			}
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				config.get('JWT_SECRET'),
+				{ expiresIn: 86400 }, // 24 hours
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	}
+);
+
+module.exports = router;
